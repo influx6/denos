f34486f6e4bf28eb3498d2ccb24ea53ef4852d02
Alex Ewetumo <trinoxf@gmail.com>
2020-03-21 18:02:33 +0800

    UPT: Adding failing/red tests

--- spec/models/cluster_spec.rb
+++ spec/models/cluster_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cluster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Cluster.new(name:'walnut', subdomain:'wal')).to be_valid
+  end
+  it "is not valid without attributes" do
+    expect(Cluster.new).to_not be_valid
+  end
+  it "is not valid without a name" do
+    expect(Cluster.new(subdomain: 'wl')).to_not be_valid
+  end
+  it "is not valid without a subdomain" do
+    expect(Cluster.new(name: 'walnut')).to_not be_valid
+  end
+  it "should not accept a subdomain longer than 5 characters" do
+    expect(Cluster.new(subdomain: 'wllsasss')).to_not be_valid
+  end
 end
--- spec/models/server_spec.rb
+++ spec/models/server_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Server, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a valid ip_string" do
+    expect(Server.new(cluster_id: 1, friendly_name: 'ny-zero')).to_not be_valid
+  end
+
+  it "is valid without a cluster_id" do
+    expect(Server.new(ip_string: '10.323.121.11', friendly_name: 'ny-zero')).to be_valid
+  end
+
+  it "is valid without a friendly name" do
+    expect(Server.new(ip_string: '10.323.121.11')).to be_valid
+  end
 end
