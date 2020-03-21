0215e9299f5acf2c7034325f9d569f2b8bb42c25
Alex Ewetumo <trinoxf@gmail.com>
2020-03-21 18:53:59 +0800

    UPT: Added validation for ip_string

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,22 @@
+IP_ADDRESS_REGEX = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
+
 class Server < ApplicationRecord
-  validates_presence_of :ip_string
+  validates :ip_string, presence: true
+  validate :only_valid_ip_addresses
+
+  def get_cluster
+    if self.cluster_id?
+      return Cluster.findById(self.clister_id)
+    end
+  end
+
+  def only_valid_ip_addresses
+    if self.ip_string == nil
+      errors.add(:ip_string, "can not be nil")
+      return
+    end
+    if self.ip_string != nil && !self.ip_string.match(IP_ADDRESS_REGEX)
+      errors.add(:ip_string, "can only be valid ipv4 addresses")
+    end
+  end
 end
