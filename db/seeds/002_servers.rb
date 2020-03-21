fc2702138449cdb14de6c95fd88b271280fbaf0a
Alex Ewetumo <trinoxf@gmail.com>
2020-03-21 16:40:11 +0800

    UPT: Updated seeds to add non-attached servers

--- db/seeds/002_servers.rb
+++ db/seeds/002_servers.rb
@@ -1,5 +1,6 @@
 require 'faker'
 
+# Create some sets of servers attached to certain clusters.
 clusters = Cluster.find_each.each do |cluster|
   rand(1..10).times do
     ip = [rand(50..300), rand(20..200), rand(60..150), rand(100..400)].join(".")
@@ -7,5 +8,13 @@ clusters = Cluster.find_each.each do |cluster|
       # nothing to see here
       puts "Created server #{server.to_json}"
     end
+  end
+end
+
+5.times do
+  ip = [rand(50..300), rand(20..200), rand(60..150), rand(100..400)].join(".")
+  Server.find_or_create_by(friendly_name: Faker::Name.unique.first_name, ip_string: ip) do | server |
+    # nothing to see here
+    puts "Created non-attached server #{server.to_json}"
   end
 end
