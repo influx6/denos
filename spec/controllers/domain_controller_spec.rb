f623eb9f653a7bd545fd73a97edb735d4a257182
Alex Ewetumo <trinoxf@gmail.com>
2020-03-22 15:05:13 +0800

    ADD: Added basic tests for controller

--- spec/controllers/domain_controller_spec.rb
+++ spec/controllers/domain_controller_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe DomainController, type: :controller do
-	describe "Pages" do 
-		pending "add teests in #{__FILE__}"
+	describe "GET index" do
+		it "should assigned @servers for page tables" do
+			server = Server.create(
+				ip_string: '10.10.10.10',
+				friendly_name: 'bar',
+			)
+
+			get :index
+			expect(assigns(:servers)).to eq([server])
+		end
+		it "should render the index template" do
+			get :index
+			expect(response).to render_template("index")
+		end
 	end
 end
