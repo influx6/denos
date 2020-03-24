4aa1a2c611b53d11bf76be290a7a80ba0dc829ae
Alex Ewetumo <trinoxf@gmail.com>
2020-03-24 23:05:07 +0800

    UPT: Updated commentary and ensured to reload records

--- app/controllers/domain_controller.rb
+++ app/controllers/domain_controller.rb
@@ -12,16 +12,20 @@ class DomainController < ApplicationController
   end
 
   def register
-    # TODO: Uncomment this if:
+    # TODO: Decide if we should comment
+    # the record reload to optimize speed.
     #
-    # We decide to allow users editing 
-    # dns records on s3 as well or spawn this into
-    # multiple instances (to scale horizontally)
-    # then we need to add uncomment this, to 
-    # ensure we consistently are dealing with 
-    # latest records from amazon.
+    # If we decide to not scale this service 
+    # and not allow users edit records on AWS 
+    # then this line can be commented as we 
+    # may safely assume all records are still
+    # the same from the last get or update.
     #
-    # AWS::Service::R53.reload_records()
+    # This does mean if our underline dns service
+    # goes down request with the old records will
+    # be made but then again those will end with 
+    # failure, so not much to worry about.
+    AWS::Service::R53.reload_records()
 
     begin
       server = Server.find(params[:id])
@@ -37,16 +41,21 @@ class DomainController < ApplicationController
   end
 
   def deregister
-    # TODO: Uncomment this if:
+    # TODO: Decide if we should comment
+    # the record reload to optimize speed.
     #
-    # We decide to allow users editing 
-    # dns records on s3 as well or spawn this into
-    # multiple instances (to scale horizontally)
-    # then we need to add uncomment this, to 
-    # ensure we consistently are dealing with 
-    # latest records from amazon.
+    # If we decide to not scale this service 
+    # and not allow users edit records on AWS 
+    # then this line can be commented as we 
+    # may safely assume all records are still
+    # the same from the last get or update.
     #
-    # AWS::Service::R53.reload_records()
+    # This does mean if our underline dns service
+    # goes down request with the old records will
+    # be made but then again those will end with 
+    # failure, so not much to worry about.
+    AWS::Service::R53.reload_records()
+
     begin
       server = Server.find(params[:id])
       AWS::Service::R53.rm_server(server)
