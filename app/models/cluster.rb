272cfbe9f057484e69eaff25c7253cbcd1345e96
Alex Ewetumo <trinoxf@gmail.com>
2020-03-31 19:48:49 +0800

    UPT: Clean up model validations

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,10 +1,7 @@
+# frozen_string_literal: true
 class Cluster < ApplicationRecord
   has_many :server
 
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
-  validates :subdomain, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 5}
-
-  def find_servers
-    return Server.where(cluster_id: self.id).order(created_at: :asc)
-  end
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :subdomain, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 5 }
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,22 +1,18 @@
+# frozen_string_literal: true
 IP_ADDRESS_REGEX = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
 
 class Server < ApplicationRecord
   belongs_to :cluster
 
-  validates :cluster_id, presence: true
-  validates :ip_string, presence: true, uniqueness: true
   validate :only_valid_ip_addresses
-
-  def get_cluster
-    return Cluster.find_by(id: self.cluster_id)
-  end
+  validates :cluster_id, presence: true
 
   def only_valid_ip_addresses
-    if self.ip_string == nil
+    if ip_string.nil?
       errors.add(:ip_string, "can not be nil")
       return
     end
-    if self.ip_string != nil && !self.ip_string.match(IP_ADDRESS_REGEX)
+    unless ip_string.match(IP_ADDRESS_REGEX)
       errors.add(:ip_string, "can only be valid ipv4 addresses")
     end
   end
