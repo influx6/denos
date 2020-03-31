544e45341d971aa8e86f24066f0567d758fa9041
Alex Ewetumo <trinoxf@gmail.com>
2020-03-31 19:52:57 +0800

    UPT: Run rubocop on config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
 require 'aws-sdk-core'
 require 'services/dns_service'
 
 Aws.config.update({
-					  stub_responses: ENV['RAILS_ENV'] == 'test',
-                      region: ENV['AWS_REGION'],
-                      credentials: Aws::Credentials.new(
-                          ENV['AWS_ACCESS_KEY_ID'],
-                          ENV['AWS_SECRET_ACCESS_KEY'],
-                      )
-                  })
+  stub_responses: ENV['RAILS_ENV'] == 'test',
+  region: ENV['AWS_REGION'],
+  credentials: Aws::Credentials.new(
+    ENV['AWS_ACCESS_KEY_ID'],
+    ENV['AWS_SECRET_ACCESS_KEY'],
+  ),
+})
 
 module AWS
   class Service
-  	R53Client = Aws::Route53::Client.new
-    R53 = DNSService::Route53::Provider.new(ENV['AWS_HOSTED_ZONE'], AWS::Service::R53Client)
+    R53Client = Aws::Route53::Client.new
+    R53 = DNSService::Provider.new(ENV['AWS_HOSTED_ZONE'], AWS::Service::R53Client)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
   get 'domain/index'
   get 'domain', to: 'domain#index'
