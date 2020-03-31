ac25b89d55cf9ec592481b0c37195f822aa21bf6
Alex Ewetumo <trinoxf@gmail.com>
2020-03-31 19:53:49 +0800

    UPT: Updated DNSServices, removed cached, make stateless

--- lib/services/dns_service.rb
+++ lib/services/dns_service.rb
@@ -1,290 +1,209 @@
+# frozen_string_literal: true
 require "aws-sdk-route53"
-
-# local imports
-require "services/resource_record"
 
 DOMAINS = /^([a-z0-9]+)[\-\.]{1}(([a-z0-9]+)*\.[a-z]{2,5})[\.]?$/
 
 module DNSService
+  DEFAULT_TTL = 300
 
-  module Route53
-    DEFAULT_TTL = 300
+  def get_domain_subdomain(resource_record)
+    domain_match = DOMAINS.match(resource_record.name)
+    if domain_match.nil?
+      raise Exception, "Domain #{resource_record.name} is invalid, expected: dl.dmm.com"
+    end
+    {
+      domain: domain_match[2],
+      subdomain: domain_match[1],
+    }
+  end
 
-    def get_domain_from_resource_record(resource_record)
-      domain_match = DOMAINS.match(resource_record.name)
-      if domain_match == nil
-        raise Exception.new "Domain #{resource_record.name} is invalid, expected: dl.dmm.com"
-      end
-      return {
-          domain: domain_match[2],
-          subdomain: domain_match[1],
-      }
+  def create_resource_record(ip)
+    Aws::Route53::Types::ResourceRecord.new(
+      value: ip,
+    )
+  end
+
+  def create_resource_record_hash(ip)
+    {
+      value: ip,
+    }
+  end
+
+  def create_record_set_hash(type, name, ttl, ips)
+    {
+      ttl: ttl,
+      type: type,
+      name: name,
+      weight: nil,
+      failover: nil,
+      geo_location: nil,
+      set_identifier: nil,
+      multi_value_answer: nil,
+      resource_records: ips,
+    }
+  end
+
+  def create_hosted_zone(domain, id)
+    {
+      data: {
+        hosted_zone: {
+          id: id,
+          name: domain,
+        },
+      },
+    }
+  end
+
+  def change_resource_set_request(action, hosted_zone, resource_record)
+    {
+      hosted_zone_id: hosted_zone.id,
+      change_batch: {
+        changes: [
+          {
+            action: action,
+            resource_record_set: resource_record,
+          },
+        ],
+      },
+    }
+  end
+
+  def record_map_list(record)
+    ips = []
+    record.resource_records.each do |v|
+      ips.push({ value: v[:value] })
+    end
+    ips
+  end
+
+  def record_ip_list(record)
+    ips = []
+    record.resource_records.each do |v|
+      ips.push(v[:value])
+    end
+    ips
+  end
+
+  def record_has_ip(record, ip_string)
+    record.resource_records.find_index { |r| r[:value] == ip_string } != nil
+  end
+
+  # Update: Remove all state cache and resolve all previous errors.
+  class Provider
+    include DNSService
+
+    def initialize(hosted_zone_id, r53_client)
+      @r53 = r53_client
+      @hosted_zone = r53_client.get_hosted_zone({ id: hosted_zone_id }).data[:hosted_zone]
     end
 
-    def create_resource_record(ip)
-      return Aws::Route53::Types::ResourceRecord.new(
-          value: ip,
-      )
-    end
+    def get_servers
+      server_records = []
+      dns_records_hash = get_resource_records
+      dns_records_hash.each_value do |rs|
+        ips = record_ip_list(rs)
+        servers = Server.where(ip_string: ips)
 
-    def create_resource_record_hash(ip)
-      return {
-          value: ip,
-      }
-    end
-
-    def create_resource_record_name(subdomain, domain)
-      return [subdomain, domain].join('.')
-    end
-
-    def create_record_set_hash(type, name, ttl, ips)
-      return {
-          ttl: ttl,
-          type: type,
-          name: name,
-          weight: nil,
-          failover: nil,
-          geo_location: nil,
-          set_identifier: nil,
-          multi_value_answer: nil,
-          resource_records: ips,
-      }
-    end
-
-    def create_hosted_zone(domain, id)
-      return {
-          data: {
-              hosted_zone: {
-                  id: id,
-                  name: domain,
-              }
-          }
-      }
-    end
-
-    def create_change_resource_set_request(action, hosted_zone, resource_record)
-      return {
-          hosted_zone_id: hosted_zone.id,
-          change_batch: {
-              changes: [
-                  {
-                      action: action,
-                      resource_record_set: resource_record,
-                  }
-              ]
-          }
-      }
-    end
-
-    # Provider takes a few short cuts to reduce multiple look ups
-    # in that it caches 
-    class Provider
-      include DNSService::Route53
-
-      def initialize(hosted_zone_id, r53Client)
-        @r53 = r53Client
-        @resource_record_cache = {}
-        @hosted_zone = r53Client.get_hosted_zone({id: hosted_zone_id}).data[:hosted_zone]
-      end
-
-      def get_records
-        # do a record reload if we have an empty map
-        # as regards registered servers.
-        if @resource_record_cache.empty?
-          self.reload_records()
-        end
-
-        # Return what we have, let user ensure to reload
-        return @resource_record_cache
-      end
-
-      def get_servers
-        records = []
-        self.get_records().each do |subdomain, rs|
-          ips = rs.to_ip_list()
-          servers = Server.where(ip_string: ips)
-
-          # check for ips which where not found
-          # roughly a O(n^2) complexity here, but
-          # it's an acceptable cost.
-          # As ips will be only ever equal to or greater servers.length)
-          ips.each do |ip|
-            server = servers.find { |s| s.ip_string == ip }
-            if server != nil
-              records.push({
-                               server: server,
-                               domain: rs.addr(),
-                               ip: server.ip_string,
-                               subdomain: rs.subdomain,
-                           })
-              next
-            end
-
-            records.push({
-                             ip: ip,
-                             server: nil,
-                             domain: rs.addr(),
-                             subdomain: rs.subdomain,
-                         })
+        # check for ips which where not found
+        # roughly a O(n^2) complexity here, but
+        # it's an acceptable cost.
+        # As ips will be only ever equal to or greater servers.length)
+        ips.each do |ip|
+          server = servers.find { |s| s.ip_string == ip }
+          unless server.nil?
+            server_records.push({ server: server, domain: rs.name, ip: server.ip_string })
+            next
           end
-        end
-        return records
-      end
 
-      def get_server_domain(server)
-        dns_records = @resource_record_cache
-        dns_record = dns_records[server.cluster.subdomain]
-
-        if dns_record == nil
-          return
-        end
-
-        if dns_record.has_ip(server.ip_string)
-          return dns_record.addr()
+          server_records.push({ ip: ip, server: nil, domain: rs.name })
         end
       end
 
-      def has_server(server)
-        dns_record = @resource_record_cache[server.cluster.subdomain]
-        if dns_record == nil
-          return false
-        end
-        return dns_record.has_ip(server.ip_string)
+      {
+        server_records: server_records,
+        subdomains_hash: dns_records_hash,
+      }
+    end
+
+    def has_server(server)
+      record = get_resource_record_for(server)
+      if record.nil?
+        return false
+      end
+      record_has_ip(record, server.ip_string)
+    end
+
+    def add_server(server)
+      unless server.valid?
+        raise "Bad server record provided"
       end
 
-      def reload_records
-        self.get_resource_records()
-      end
+      domain_name = [server.cluster.subdomain, @hosted_zone.name].join('.')
+      record = get_resource_record_for(server)
 
-      def add_server(server)
-        if @resource_record_cache.empty?
-          self.reload_records()
-        end
-
-        if !server.valid?
-          return false
-        end
-
-        if !server.cluster.valid?
-          return false
-        end
-
-        local_rr = nil
-        local_record = @resource_record_cache[server.cluster.subdomain]
-
-        domain_name = create_resource_record_name(server.cluster.subdomain, @hosted_zone.name)
-        if local_record == nil
-          domain_rs = create_resource_record_hash(server.ip_string)
-
-          local_rr = create_record_set_hash('A', domain_name, DEFAULT_TTL, [domain_rs])
-          local_record = ResourceRecord.new(local_rr, @hosted_zone.name, server.cluster.subdomain)
-
-          # add to in-memory cache and list
-          @resource_record_cache[server.cluster.subdomain] = local_record
-        else
-          local_record.add_ip(server.ip_string)
-          local_rr = create_record_set_hash('A', domain_name, DEFAULT_TTL, local_record.to_map_list())
-        end
-
-        # send update request to aws.
-        # keep it simple, send one at a time.
-        crr = create_change_resource_set_request('UPSERT', @hosted_zone, local_rr)
-
-        # am not sure we need to ChangeSet returned
+      if record.nil?
+        domain_rs = create_resource_record_hash(server.ip_string)
+        local_rr = create_record_set_hash('A', domain_name, DEFAULT_TTL, [domain_rs])
+        crr = change_resource_set_request('UPSERT', @hosted_zone, local_rr)
         @r53.change_resource_record_sets(crr)
-
-        self.reload_records()
-        return true
+        return
       end
 
-      def rm_server(server)
-        if @resource_record_cache.empty?
-          self.reload_records()
-        end
-
-        if !server.valid?
-          return false
-        end
-
-        if !server.cluster.valid?
-          return false
-        end
-
-        local_record = @resource_record_cache[server.cluster.subdomain]
-        if local_record == nil
-          return false
-        end
-
-
-        action = 'UPSERT'
-        if local_record.rr.resource_records.size == 1
-          action = 'DELETE'
-        else
-          local_record.rm_ip(server.ip_string)
-        end
-
-        domain_name = create_resource_record_name(local_record.subdomain, @hosted_zone.name)
-        local_rr = create_record_set_hash('A', domain_name, DEFAULT_TTL, local_record.to_map_list())
-
-        # send update request to aws.
-        # keep it simple, send one at a time.
-        crr = create_change_resource_set_request(action, @hosted_zone, local_rr)
-
-        # am not sure we need to ChangeSet returned
-        @r53.change_resource_record_sets(crr)
-
-        if action == 'DELETE'
-          @resource_record_cache.delete(server.cluster.subdomain)
-        end
-
-        self.reload_records()
-        return true
+      if record_has_ip(record, server.ip_string)
+        return
       end
 
-      protected
+      record.resource_records.push(Aws::Route53::Types::ResourceRecord.new(value: server.ip_string))
+      local_rr = create_record_set_hash('A', domain_name, DEFAULT_TTL, record_map_list(record))
+      crr = change_resource_set_request('UPSERT', @hosted_zone, local_rr)
+      @r53.change_resource_record_sets(crr)
+    end
 
-      # NOTE(reviewer): Considering my test data is 
-      # intentionally made to be below 100, I wont run
-      # into issues having to paginate. But I am not
-      # sure if the test project intended to see how I would
-      # handle this as well.
-      #
-      # If so, then I would need to update this to keep track
-      # of the last records and type wanted (which is A records)
-      # then use the `start_with_name` and `start_with_type` to 
-      # paginate when records go beyond 100.
-      #
-      def get_resource_records
-        response = @r53.list_resource_record_sets({
-                                                      hosted_zone_id: @hosted_zone.id,
-                                                  })
-
-        new = {}
-        old = @resource_record_cache
-        response.resource_record_sets.each do |record|
-          if record.type == 'A'
-            rr_data = get_domain_from_resource_record(record)
-
-            rr = nil
-            if old.key?(rr_data[:subdomain])
-              rr = old[rr_data[:subdomain]]
-              rr.use_rr(record)
-            else
-              rr = ResourceRecord.new(record, rr_data[:domain], rr_data[:subdomain])
-            end
-
-            # Amazon enforces one subdomain or value 
-            # in route53, so you can't have say two resource
-            # record set of same sub-domain. So we good here.
-            # No need to worry about 2 records of same sub-domain.
-            new[rr_data[:subdomain]] = rr
-          end
-        end
-
-        # update cache with new set.
-        @resource_record_cache = new
+    def rm_server(server)
+      unless server.valid?
+        raise "Bad server record provided"
       end
 
+      record = get_resource_record_for(server)
+      unless record_has_ip(record, server.ip_string)
+        return
+      end
+
+      action = 'UPSERT'
+      if record.resource_records.size == 1
+        action = 'DELETE'
+      else
+        index = record.resource_records.find_index { |r| r[:value] == server.ip_string }
+        record.resource_records.delete_at(index)
+      end
+
+      local_rr = create_record_set_hash('A', record.name, DEFAULT_TTL, record_map_list(record))
+      crr = change_resource_set_request(action, @hosted_zone, local_rr)
+      @r53.change_resource_record_sets(crr)
+    end
+
+    def get_resource_records
+      record_set = {}
+      response = @r53.list_resource_record_sets({ hosted_zone_id: @hosted_zone.id })
+      response.resource_record_sets.each do |record|
+        if record.type == 'A'
+          rr_data = get_domain_subdomain(record)
+          record_set[rr_data[:subdomain]] = record
+        end
+      end
+
+      record_set
+    end
+
+    def get_resource_record_for(server)
+      server_cluster_address = [server.cluster.subdomain, @hosted_zone.name].join('.')
+      response = @r53.list_resource_record_sets({ hosted_zone_id: @hosted_zone.id, start_record_name: server_cluster_address, start_record_type: 'A', max_items: 1 })
+
+      unless !response.resource_record_sets.empty?
+        return nil
+      end
+
+      response.resource_record_sets[0]
     end
   end
 end
