589758fc4c0c859082deab0617980ad144df89ca
Alex Ewetumo <trinoxf@gmail.com>
2020-03-31 19:49:51 +0800

    UPT: Updated views with new helper arguments

--- app/helpers/domain_helper.rb
+++ app/helpers/domain_helper.rb
@@ -1,9 +1,19 @@
+# frozen_string_literal: true
+require 'services/dns_service'
+
 module DomainHelper
-  def is_registered(server)
-    return AWS::Service::R53.has_server(server)
+  include DNSService
+
+  def is_registered(records, server)
+    record = records[server.cluster.subdomain]
+    if record.nil?
+      return false
+    end
+    record_has_ip(record, server.ip_string)
   end
 
-  def registered_domain(server)
-    return AWS::Service::R53.get_server_domain(server)
+  def registered_domain(records, server)
+    record = records[server.cluster.subdomain]
+    record&.name
   end
 end
